ca0a3f38fcef2498f40822ce4ccfa486156d8018
loveKino <842914439@qq.com>
2017-03-08 23:42:01 +0800

    remove comment

--- example/fixture/ret/demo0.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- */
-
-;(function () {
-        var __exportsVariable = require('/Users/yuer/workspaceforme/category/career/container/opensource/defcomment/src/unit').exportsVariable;
-        
-    })();
-    
--- example/test2.js
+++
@@ -1,7 +0,0 @@
-var quick = require('../ext/quick');
-
-quick({
-    srcDir: __dirname + '/fixture/src',
-    distDir: __dirname + '/fixture/ret',
-    testDir: __dirname + '/fixture/test'
-});
