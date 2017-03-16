3f86b1e841f16afdd39d2e2e26f3d7aa9000c4d9
loveKino <842914439@qq.com>
2017-03-16 18:47:05 +0800

    record line break

--- src/parseComment.js
+++ src/parseComment.js
@@ -65,15 +65,13 @@ let wipeCommentSymbols = (comment) => {
     for (let i = 0; i < lines.length; i++) {
         let line = lines[i];
         line = line.replace(/^\s+/, '');
-        if (line) {
-            while (line[0] === COMMENT_PREFIX_ADDITION) {
+        while (line[0] === COMMENT_PREFIX_ADDITION) {
+            line = line.substring(1);
+            if (line[0] === ' ') { // wipe first blank
                 line = line.substring(1);
-                if (line[0] === ' ') { // wipe first blank
-                    line = line.substring(1);
-                }
             }
-            if (line) newLines.push(line);
         }
+        if (line) newLines.push(line);
     }
     return newLines;
 };
